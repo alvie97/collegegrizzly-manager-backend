a6b4b9775f9b806a3c7000865482a2533e81c13a
alfredo <alvie97m@gmail.com>
2018-06-28 19:28:08 -0400

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy   import SQLAlchemy
 from flask_migrate      import Migrate
 from config             import Config
 from flask_restful      import Api
+from flask_cors         import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
 api = Api(prefix='/api')
+cors = CORS(resources={r"/api/*": {"origins": "*"}})
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -14,6 +16,7 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    cors.init_app(app)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
