f703d6f53cba8705e9772d7fab606e58d6f8404d
alfredo <alvie97m@gmail.com>
2018-08-02 21:33:33 -0400

    us territories, counties, cities... processing script

---
+++ scripts/us_states_prcss.py
@@ -0,0 +1,68 @@
+from openpyxl import load_workbook
+
+wb = load_workbook("../../imp_misc_files/all-geocodes-v2016.xlsx")
+ws = wb.active
+
+STATE_CODE = 1
+COUNTY_CODE = 2
+COUNTY_SUBDIVISION = 3
+PLACE_CODE = 4
+CONSOLIDATED_CITY = 5
+NAME = 6
+codes = {}
+
+for row in ws.iter_rows(min_row=6):
+
+    state = row[STATE_CODE].value
+    if not state == "00":
+        if state not in codes:
+            name = row[NAME].value
+            codes[state] = {
+                "name": name,
+                "counties": {},
+                "places": {},
+                "consolidated_cities": {}
+            }
+            continue
+
+        county = row[COUNTY_CODE].value
+        if not county == "000":
+            if county not in codes[state]["counties"]:
+                name = row[NAME].value
+                codes[state]["counties"][county] = {
+                    "name": name,
+                    "subdivisions": {}
+                }
+                continue
+
+            county_subdiv = row[COUNTY_SUBDIVISION].value
+            if not county_subdiv == "000000":
+                name = row[NAME].value
+                codes[state]["counties"][county] \
+                    ["subdivisions"][county_subdiv] = name
+                continue
+
+        place = row[PLACE_CODE].value
+        if not place == "00000":
+            name = row[NAME].value
+            codes[state]["places"][place] = name
+            continue
+
+        consolidated_city = row[CONSOLIDATED_CITY].value
+        if not consolidated_city == "00000":
+            name = row[NAME].value
+            codes[state]["consolidated_cities"][consolidated_city] = name
+
+def get_consolidated_cities(states):
+    for state_code, state in states.items():
+        for city_code, city in state["consolidated_cities"].items():
+            print("city: {} located in state: {}".format(city, state["name"]))
+
+def get_counties_of(state_name, states):
+    for state_code, state in states.items():
+        if state["name"] == state_name:
+            for county_code, county in state["counties"].items():
+                print("county: {} of {}".format(county["name"], state_name))
+
+            print("number of counties: {}".format(len(state["counties"])))
+            return
