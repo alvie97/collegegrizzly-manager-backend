c82b3ea7d5ce2c287576abed3552fea16ae845a8
alfredo <alvie04vm@gmail.com>
2018-10-21 12:37:10 -0300

    pagination change college model

--- app/models/college.py
+++ app/models/college.py
@@ -13,135 +13,135 @@ from typing import List
 
 class College(PaginatedAPIMixin, LocationMixin, LocationBlacklistMixin,
               DateAudit, BaseMixin, db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  public_id = db.Column(db.String(50), unique=True)
-  name = db.Column(db.String(256))
-  room_and_board = db.Column(db.Numeric(8, 2), default=0)
-  type_of_institution = db.Column(db.String(256), nullable=True)
-  phone = db.Column(db.String(256), nullable=True)
-  website = db.Column(db.Text, nullable=True)
-  in_state_tuition = db.Column(db.Numeric(8, 2), default=0)
-  out_of_state_tuition = db.Column(db.Numeric(8, 2), default=0)
-  location = db.Column(db.String(256), nullable=True)
-  religious_affiliation = db.Column(db.String(256), nullable=True)
-  setting = db.Column(db.String(256), nullable=True)
-  number_of_students = db.Column(db.Integer, default=0)
-  unweighted_hs_gpa = db.Column(db.Numeric(4, 2), default=0)
-  sat = db.Column(db.Integer, default=0)
-  act = db.Column(db.Integer, default=0)
-  scholarships = db.relationship(
-      "Scholarship",
-      backref="college",
-      cascade="all, delete-orphan",
-      lazy="dynamic")
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(256))
+    room_and_board = db.Column(db.Numeric(8, 2), default=0)
+    type_of_institution = db.Column(db.String(256), nullable=True)
+    phone = db.Column(db.String(256), nullable=True)
+    website = db.Column(db.Text, nullable=True)
+    in_state_tuition = db.Column(db.Numeric(8, 2), default=0)
+    out_of_state_tuition = db.Column(db.Numeric(8, 2), default=0)
+    location = db.Column(db.String(256), nullable=True)
+    religious_affiliation = db.Column(db.String(256), nullable=True)
+    setting = db.Column(db.String(256), nullable=True)
+    number_of_students = db.Column(db.Integer, default=0)
+    unweighted_hs_gpa = db.Column(db.Numeric(4, 2), default=0)
+    sat = db.Column(db.Integer, default=0)
+    act = db.Column(db.Integer, default=0)
+    scholarships = db.relationship(
+        "Scholarship",
+        backref="college",
+        cascade="all, delete-orphan",
+        lazy="dynamic")
 
-  pictures = db.relationship(
-      "Picture",
-      backref="college",
-      cascade="all, delete-orphan",
-      lazy="dynamic")
+    pictures = db.relationship(
+        "Picture",
+        backref="college",
+        cascade="all, delete-orphan",
+        lazy="dynamic")
 
-  majors = db.relationship(
-      "Major",
-      secondary=college_major,
-      backref=db.backref("colleges", lazy="dynamic"),
-      lazy="dynamic")
+    majors = db.relationship(
+        "Major",
+        secondary=college_major,
+        backref=db.backref("colleges", lazy="dynamic"),
+        lazy="dynamic")
 
-  ATTR_FIELDS = [
-      "name", "room_and_board", "type_of_institution", "phone", "website",
-      "in_state_tuition", "out_of_state_tuition", "location",
-      "religious_affiliation", "setting", "number_of_students",
-      "unweighted_hs_gpa", "sat", "act"
-  ]
+    ATTR_FIELDS = [
+        "name", "room_and_board", "type_of_institution", "phone", "website",
+        "in_state_tuition", "out_of_state_tuition", "location",
+        "religious_affiliation", "setting", "number_of_students",
+        "unweighted_hs_gpa", "sat", "act"
+    ]
 
-  def __repr__(self):
-    return "<College {}>".format(self.name)
+    def __repr__(self):
+        return "<College {}>".format(self.name)
 
-  def add_major(self, major: Major):
-    if not self.has_major(major.name):
-      self.majors.append(major)
+    def add_major(self, major: Major):
+        if not self.has_major(major.name):
+            self.majors.append(major)
 
-  def remove_major(self, major: Major):
-    if self.has_major(major.name):
-      self.majors.remove(major)
+    def remove_major(self, major: Major):
+        if self.has_major(major.name):
+            self.majors.remove(major)
 
-  def has_major(self, major_name: str) -> bool:
-    return self.majors.filter(Major.name == major_name).count() > 0
+    def has_major(self, major_name: str) -> bool:
+        return self.majors.filter(Major.name == major_name).count() > 0
 
-  def get_majors(self) -> List[dict]:
+    def get_majors(self) -> List[dict]:
 
-    return [major.to_dict() for major in self.majors.all()]
+        return [major.to_dict() for major in self.majors.all()]
 
-  def get_avatar(self, size: int) -> str:
-    digest = md5("test@email.com".encode("utf-8")).hexdigest()
-    return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
-        digest, size)
+    def get_avatar(self, size: int) -> str:
+        digest = md5("test@email.com".encode("utf-8")).hexdigest()
+        return "https://www.gravatar.com/avatar/{}?d=identicon&s={}".format(
+            digest, size)
 
-  def total_ofs(self) -> float:
-    return self.room_and_board + self.out_of_state_tuition
+    def total_ofs(self) -> float:
+        return self.room_and_board + self.out_of_state_tuition
 
-  def total_is(self) -> float:
-    return self.room_and_board + self.in_state_tuition
+    def total_is(self) -> float:
+        return self.room_and_board + self.in_state_tuition
 
-  def get_logo(self) -> str:
-    logo = self.pictures.filter_by(type="logo").first()
+    def get_logo(self) -> str:
+        logo = self.pictures.filter_by(type="logo").first()
 
-    if logo is None:
-      return self.get_avatar(128)
+        if logo is None:
+            return self.get_avatar(128)
 
-    return photos.url(logo.name)
+        return photos.url(logo.name)
 
-  def delete(self):
-    pics = self.pictures.all()
+    def delete(self):
+        pics = self.pictures.all()
 
-    for pic in pics:
-      pic.delete()
+        for pic in pics:
+            pic.delete()
 
-    db.session.delete(self)
+        db.session.delete(self)
 
-  def for_pagination(self):
-    return {
-        "name": self.name,
-        "public_id": self.public_id,
-        "audit_dates": self.audit_dates(),
-        "logo": self.get_logo(),
-        "_links": {
-            "get_college":
-                url_for("colleges.get_college", college_id=self.public_id)
-        }
-    }
-
-  def to_dict(self):
-    return {
-        "public_id": self.public_id,
-        "editable_fields": {
+    def for_pagination(self):
+        return {
             "name": self.name,
-            "room_and_board": str(self.room_and_board),
-            "type_of_institution": self.type_of_institution,
-            "phone": self.phone,
-            "website": self.website,
-            "in_state_tuition": str(self.in_state_tuition),
-            "out_of_state_tuition": str(self.out_of_state_tuition),
-            "location": self.location,
-            "religious_affiliation": self.religious_affiliation,
-            "setting": self.setting,
-            "number_of_students": self.number_of_students,
-            "unweighted_hs_gpa": str(self.unweighted_hs_gpa),
-            "sat": self.sat,
-            "act": self.act
-        },
-        "audit_dates": self.audit_dates(),
-        "logo": self.get_logo(),
-        "majors": self.get_majors(),
-        "_links": {
-            "scholarships":
+            "public_id": self.public_id,
+            **self.audit_dates(), 
+            "logo": self.get_logo(),
+            "_links": {
+                "get_college":
+                url_for("colleges.get_college", college_id=self.public_id)
+            }
+        }
+
+    def to_dict(self):
+        return {
+            "public_id": self.public_id,
+            "editable_fields": {
+                "name": self.name,
+                "room_and_board": str(self.room_and_board),
+                "type_of_institution": self.type_of_institution,
+                "phone": self.phone,
+                "website": self.website,
+                "in_state_tuition": str(self.in_state_tuition),
+                "out_of_state_tuition": str(self.out_of_state_tuition),
+                "location": self.location,
+                "religious_affiliation": self.religious_affiliation,
+                "setting": self.setting,
+                "number_of_students": self.number_of_students,
+                "unweighted_hs_gpa": str(self.unweighted_hs_gpa),
+                "sat": self.sat,
+                "act": self.act
+            },
+            "audit_dates": self.audit_dates(),
+            "logo": self.get_logo(),
+            "majors": self.get_majors(),
+            "_links": {
+                "scholarships":
                 url_for(
                     "colleges.get_college_scholarships",
                     college_id=self.public_id),
-            "pictures":
+                "pictures":
                 url_for("files.get_pictures", college_id=self.public_id),
-            "in_state_requirement":
+                "in_state_requirement":
                 self.location_requirement_endpoints(
                     "colleges.college", college_id=self.public_id)
+            }
         }
-    }
