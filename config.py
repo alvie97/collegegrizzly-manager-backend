477b777bc5290a55a965d4e868997f9308339126
alfredo <alvie97m@gmail.com>
2019-01-07 12:03:57 -0300

    csrf cookie name

--- config.py
+++ config.py
@@ -7,26 +7,26 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-  SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-  SQLALCHEMY_DATABASE_URI = os.environ.get(
-      'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
-  SECURE_TOKEN_COOKIES = False
-  JWT_SECRET = os.environ.get('SECRET_KEY') or 'my-secret-never-guess'
-  JWT_ALGORITHM = "HS256"
-  ACCESS_COOKIE_NAME = "actk"
-  REFRESH_COOKIE_NAME = "rftk"
-  REFRESH_TOKEN_DURATION = timedelta(days=30)
-  REFRESH_COOKIE_EXPIRATION = timedelta(days=31)
-  CSRF_COOKIE_NAME = "x-csrf-token"
-  MAIL_SERVER = os.environ.get('MAIL_SERVER')
-  MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-  MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-  MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-  MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-  ADMINS = ['alvie97m@gmail.com']
-  COLLEGES_PER_PAGE = 5
-  SCHOLARSHIPS_PER_PAGE = 5
-  PER_PAGE = 5
-  UPLOADED_PHOTOS_DEST = 'static/photos/'
-  UPLOADED_PHOTOS_URL = 'http://localhost:5000/api/files/photos/'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECURE_TOKEN_COOKIES = False
+    JWT_SECRET = os.environ.get('SECRET_KEY') or 'my-secret-never-guess'
+    JWT_ALGORITHM = "HS256"
+    ACCESS_COOKIE_NAME = "actk"
+    REFRESH_COOKIE_NAME = "rftk"
+    REFRESH_TOKEN_DURATION = timedelta(days=30)
+    REFRESH_COOKIE_EXPIRATION = timedelta(days=31)
+    CSRF_COOKIE_NAME = "X-CSRF-TOKEN"
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['alvie97m@gmail.com']
+    COLLEGES_PER_PAGE = 5
+    SCHOLARSHIPS_PER_PAGE = 5
+    PER_PAGE = 5
+    UPLOADED_PHOTOS_DEST = 'static/photos/'
+    UPLOADED_PHOTOS_URL = 'http://localhost:5000/api/files/photos/'
